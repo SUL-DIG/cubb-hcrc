627fe489b03d47c38c48e5bdceb912215b8b178e
Jacqueline Hettel <hettelj@stanford.edu>
2013-10-21 07:37:01 -0700

    Creating subject links.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+def render_external_link args, results = Array.new
+		text = args[:document].get(blacklight_config.show_fields[args[:field]][:text])
+        url = args[:document].get(args[:field])
+        results << link_to(text, 'http://hcrc-dev.stanford.edu/?f%5Bkeywords_facet%5D%5B%5D='url, { :target => "_blank" }).html_safe
+  end
+
 end
