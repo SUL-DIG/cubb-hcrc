fd89a72136ec94898eb7b4a1518c1ba32b933fc6
Jacqueline Hettel <hettelj@stanford.edu>
2013-10-21 08:07:31 -0700

    Configuring subject links.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 def render_external_link args, results = Array.new
 		text = args[:document].get(blacklight_config.show_fields[args[:field]][:text])
-		url_pre = "/?f%5Bkeywords_facet%5D%5B%5D="
+		url_pre = args[:document].get(blacklight_config.facet_fields[args[:field])
 		url_post = args[:document].get(args[:field])
         results << link_to(text, (url_pre + url_post), { :target => "_self" }).html_safe
   end
