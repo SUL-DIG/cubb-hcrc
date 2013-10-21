2c2e781551227064b01cee3d2aa19652b74f1654
Jacqueline Hettel <hettelj@stanford.edu>
2013-10-21 08:24:38 -0700

    Configuring subject links.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,10 @@
 module ApplicationHelper
 def render_external_link args, results = Array.new
 		text = args[:document].get(blacklight_config.show_fields[args[:field]][:text])
-		url_post = args[:document].get(args[:field])
-        results << link_to(text, url_post, { :target => "_self" }).html_safe
+		link_text = text.split (',')
+		url_pre = args[:document].get(args[:field])
+		url_post = url_pre.split
+        results << link_to(link_text, url_post, { :target => "_self" }).html_safe
   end
 
 end
