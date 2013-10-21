33cb5295ee8a259d6859c4fce23894a8dd9a1513
Jacqueline Hettel <hettelj@stanford.edu>
2013-10-21 07:59:43 -0700

    Configuring subject links.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,7 @@
 module ApplicationHelper
 def render_external_link args, results = Array.new
 		link_text = args[:document].get(blacklight_config.show_fields[args[:field]][:text])
-        url = "hcrc-dev.stanford.edu/?f%5Bkeywords_facet%5D%5B%5D=" + args[:document].get(args[:field])
-        results << link_to(link_text, url, { :target => "_blank" }).html_safe
+        results << link_to(link_text, ("/?f%5Bkeywords_facet%5D%5B%5D=" + args[:document].get(args[:field])), { :target => "_blank" }).html_safe
   end
 
 end
