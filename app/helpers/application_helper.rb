2be934915c516454084be2889346bc1033ca57dc
Jacqueline Hettel <hettelj@stanford.edu>
2013-10-21 07:49:08 -0700

    Configuring subject links.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 def render_external_link args, results = Array.new
 		text = args[:document].get(blacklight_config.show_fields[args[:field]][:text])
         url = args[:document].get(args[:field])
-        results << link_to(text, url, { :target => "_blank" }).html_safe
+        results << link_to(text, catalog_index_path.url, { :target => "_blank" }).html_safe
   end
 
 end
