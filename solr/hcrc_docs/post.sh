9c9ee3d0e5048d211102099ea601dd82ede86606
Jacqueline Hettel <hettelj@stanford.edu>
2013-11-25 08:23:30 -0800

    Updating URLs for prod server.

--- solr/hcrc_docs/post.sh
+++ solr/hcrc_docs/post.sh
@@ -15,7 +15,7 @@
 # limitations under the License.
 
 FILES=$*
-URL=https://sul-solr.stanford.edu/solr/dig_hcrc_test/update
+URL=https://sul-solr.stanford.edu/solr/dig_hcrc_prod/update
 
 for f in $FILES; do
   echo Posting file $f to $URL
